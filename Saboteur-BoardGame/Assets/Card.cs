01a994196d8a02aeb1d6d0071ea5fef7e235879d
zeotuan <zeotuan@gmail.com>
2019-07-19 18:24:24 +1000

    create plane, card canvas

---
+++ Saboteur-BoardGame/Assets/Card.cs
@@ -0,0 +1,21 @@
+﻿//testing script wrote for references (does not work)
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int right,left,up,down,rotate;
+    public Image frontcardArtWork;
+    public Image backCardArtWork;
+    public Text  cardName;
+
+    // card placing rule 
+    public bool ValidMove(Card[.] board, int x1, int y1, int  x2, int y2){
+        if(board[x2,y2]!= null){// cannot place card on top of other card 
+            return  false;
+        }
+
+        if()
+    }
+}
