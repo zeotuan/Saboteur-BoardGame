62196b02a1ec9d62f245b300f65a304ec0b98988
mugi0GH <jieng0805@gmail.com>
2019-09-16 17:49:10 +1000

    1stUI.2
    Change it to unity 2.0
    redo the prefab of card
    re-edit the scripts.
    added Horizontal layout group
    the script for the user is able to drag the card outside of the hand.

--- Saboteur-BoardGame1/BoardGame/Assets/Scripts/7.InGame Scene/Draggable.cs
+++ Saboteur-BoardGame1/BoardGame/Assets/Scripts/7.InGame Scene/Draggable.cs
@@ -3,27 +3,26 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler
 {
-    
-    public void Start()
-    {
-        
-    }
+    public Transform parentToReturnTo = null;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
 
         this.transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
-        
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
 }
---
+++ Saboteur-BoardGame1/BoardGame/Assets/Scripts/7.InGame Scene/DropZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour,IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)//检测鼠标进入
+    {
+        Debug.Log("OnPointerEnter to " + gameObject.name);
+    }
+    public void OnPointerExit(PointerEventData eventData)//检测鼠标离开
+    {
+        Debug.Log("OnPointerExit to " + gameObject.name);
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+        Draggable d =  eventData.pointerDrag.GetComponent<Draggable>();
+        if(d!=null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+}
