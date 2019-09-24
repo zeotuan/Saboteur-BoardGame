de8ffc2e1a50a1e2f1ceb35ecd469085599fa9b7
404-Null <53752166+404-Null@users.noreply.github.com>
2019-09-24 14:00:53 +1000

    steve assign prefab for board generation

--- Saboteur-BoardGame1/BoardGame/Assets/Back End Script/Board.cs
+++ Saboteur-BoardGame1/BoardGame/Assets/Back End Script/Board.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.UI;
 public class Board : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     //2 dimensional array for cards on board 
