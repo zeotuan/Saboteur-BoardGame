03fe5ec5935f2f59f3a8083f6a6b6c3ab3759474
mugi0GH <jieng0805@gmail.com>
2019-10-20 16:23:51 +1100

    board bug fixed

--- Saboteur-BoardGame1/BoardGame/Assets/Back End Script/Board.cs
+++ Saboteur-BoardGame1/BoardGame/Assets/Back End Script/Board.cs
@@ -64,89 +64,43 @@ public class Board : MonoBehaviour
 
     public void GenerateBoard(int numPlayer, int maxCol, int maxRow)
     {
+        //Destroy existed GridObject
         board = new GameObject[maxRow, maxCol];
-
         if (board.Length > 0)
         {
             for (int r = 0; r < maxRow; r++)
-
-        if(board.Length > 0){
-            for (int r = 0; r < maxRow; r++) { 
-                for(int c = 0; c < maxCol; c++)
-                {
-                    Destroy(board[r,c]);  
-                }
-            }
-        }
-        shuffleDestination();
-        //GameManager.Instance.shuffle(DesGrid);
-        for (int r = 0; r < maxRow; r++) { 
-            for(int c = 0; c < maxCol; c++)
-
             {
                 for (int c = 0; c < maxCol; c++)
                 {
                     Destroy(board[r, c]);
                 }
             }
-            shuffleDestination();
-            //GameManager.Instance.shuffle(DesGrid);
-            for (int r = 0; r < maxRow; r++)
-            {
-                for (int c = 0; c < maxCol; c++)
-                {
-
-                    GameObject tile;
-                    if (r == 2 && c == 0)
-                    {
-                        tile = Instantiate(startGrid) as GameObject;
-                    }
-                    else if (r == 0 && c == 8)
-                    {
-                        tile = Instantiate(DesGrid[0]) as GameObject;
-                    }
-                    else if (r == 2 && c == 8)
-                    {
-                        tile = Instantiate(DesGrid[1]) as GameObject;
-                    }
-                    else if (r == 4 && c == 8)
-                    {
-                        tile = Instantiate(DesGrid[2]) as GameObject;
-                    }
-                    else
-                    {
-                        tile = Instantiate(GridPrefab) as GameObject;
-                    }
-
-
-                    if (tile.name == "TrueDes Variant(Clone)")
-                    {
-                        xTrueDes = r;
-                        yTrueDes = c;
-                    }
-
-
-
-                    float posX = c * tileSize;
-                    float posY = r * tileSize;
-                    tile.transform.position = new Vector3(posX, posY, transform.position.z);
-                    tile.transform.SetParent(transform);
-                    board[r, c] = tile;
-                    tile.GetComponent<Property>().x = r;
-                    tile.GetComponent<Property>().y = c;
-                }
-
-            }
-
-            //float gridH = maxCol * tileSize;
-            //float gridW = maxRow * tileSize;
-
-
-
-            //transform.position = new Vector3(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
-
         }
-=======
+        shuffleDestination();
+        //GameManager.Instance.shuffle(DesGrid);
+        for (int r = 0; r < maxRow; r++)
+        {
+            for (int c = 0; c < maxCol; c++)
+            {
+                GameObject tile;
+                if (r == 2 && c == 0)
+                {
+                    tile = Instantiate(startGrid) as GameObject;
+                }
+                else if (r == 0 && c == 8)
+                {
+                    tile = Instantiate(DesGrid[0]) as GameObject;
+                }
+                else if (r == 2 && c == 8)
+                {
+                    tile = Instantiate(DesGrid[1]) as GameObject;
+                }
+                else if (r == 4 && c == 8)
+                {
+                    tile = Instantiate(DesGrid[2]) as GameObject;
+                }
+                else
+                {
                     tile = Instantiate(GridPrefab) as GameObject;
                 }
                 if (tile.name == "TrueDes Variant(Clone)")
@@ -161,10 +115,9 @@ public class Board : MonoBehaviour
                 board[r, c] = tile;
                 tile.GetComponent<Property>().x = r;
                 tile.GetComponent<Property>().y = c;
-            }  
+            }
         }
-        
->>>>>>> b002ec5b9bde90da2c07e953ff0ee0d7659affde
+
     }
 
     public GameObject findNearestGrid(GameObject card)
@@ -207,13 +160,13 @@ public class Board : MonoBehaviour
             DesGrid[randomIndex] = container;
         }
     }
-     
+
     public PlayerController chooseTarget(PlayerController target)
     {
         return target;
     }
 
-    public void setGrid(int x, int y, Sprite img,Property setProperty)
+    public void setGrid(int x, int y, Sprite img, Property setProperty)
     {
         Property grid = board[x, y].GetComponent<Property>();
         grid.used = true;
@@ -246,22 +199,22 @@ public class Board : MonoBehaviour
         bool[,] visited = new bool[5, 9];
 
         Queue<Property> queue = new Queue<Property>();
-        queue.Enqueue(board[x,y].GetComponent<Property>());
-        
-        while(queue.Count > 0)
+        queue.Enqueue(board[x, y].GetComponent<Property>());
+
+        while (queue.Count > 0)
         {
             Property current = queue.Dequeue();
             visited[current.x, current.y] = true;
             Debug.Log("current: " + current.x + " " + current.y);
             List<Property> ReachableList = Getreachable(current);
-            foreach(Property next in ReachableList)
+            foreach (Property next in ReachableList)
             {
-                
+
                 if (next.x == xTrueDes && next.y == yTrueDes)
                 {
                     return true;
                 }
-                if(!visited[next.x, next.y])
+                if (!visited[next.x, next.y])
                 {
                     queue.Enqueue(next);
                 }
@@ -278,7 +231,7 @@ public class Board : MonoBehaviour
         if (property.x != 0)
         {
             up = board[property.x - 1, property.y].GetComponent<Property>();
-            if(up.Down && property.Up && up.center)//need to check center since though without the center will not lead to anywhere else even thought path can still be placed next to them  
+            if (up.Down && property.Up && up.center)//need to check center since though without the center will not lead to anywhere else even thought path can still be placed next to them  
             {
                 reachableList.Add(up);
                 Debug.Log("reachable: " + up.x + " " + up.y);
@@ -287,7 +240,7 @@ public class Board : MonoBehaviour
 
         if (property.y != 8)
         {
-            right = board[property.x, property.y+1].GetComponent<Property>();
+            right = board[property.x, property.y + 1].GetComponent<Property>();
             if (right.Left && property.Right && right.center)
             {
                 reachableList.Add(right);
@@ -296,14 +249,14 @@ public class Board : MonoBehaviour
         }
         if (property.y != 0)
         {
-            left = board[property.x, property.y-1].GetComponent<Property>();
+            left = board[property.x, property.y - 1].GetComponent<Property>();
             if (left.Right && property.Left && left.center)
             {
                 reachableList.Add(left);
                 Debug.Log("reachable: " + left.x + " " + left.y);
             }
         }
-        if (property.x!=4)
+        if (property.x != 4)
         {
             down = board[property.x + 1, property.y].GetComponent<Property>();
             if (down.Up && property.Down && down.center)
@@ -316,14 +269,18 @@ public class Board : MonoBehaviour
     }
 
     //get possible available position that can placed card above it 
-    public List<Property> GetPossibleUpPosition(){
+    public List<Property> GetPossibleUpPosition()
+    {
         List<Property> possibleUpList = new List<Property>();
-        foreach(Property prop in usedGridproperty){
-            if(prop.x==0){
+        foreach (Property prop in usedGridproperty)
+        {
+            if (prop.x == 0)
+            {
                 continue;
             }
-            Property upGrid = board[prop.x-1,prop.y].GetComponent<Property>();
-            if(prop.Up && !upGrid.used){//there a way up and up is not used
+            Property upGrid = board[prop.x - 1, prop.y].GetComponent<Property>();
+            if (prop.Up && !upGrid.used)
+            {//there a way up and up is not used
                 possibleUpList.Add(upGrid);
             }
         }
@@ -331,14 +288,18 @@ public class Board : MonoBehaviour
     }
 
     //get possible available position that can placed card below it 
-    public List<Property> GetPossibleDownPosition( ){
+    public List<Property> GetPossibleDownPosition()
+    {
         List<Property> possibleDownList = new List<Property>();
-        foreach(Property prop in usedGridproperty){
-            if(prop.x == 4){
+        foreach (Property prop in usedGridproperty)
+        {
+            if (prop.x == 4)
+            {
                 continue;
             }
-            Property downGrid = board[prop.x+1,prop.y].GetComponent<Property>();
-            if(prop.Up && !downGrid.used){//there a way up and up is not used
+            Property downGrid = board[prop.x + 1, prop.y].GetComponent<Property>();
+            if (prop.Up && !downGrid.used)
+            {//there a way up and up is not used
                 possibleDownList.Add(downGrid);
             }
         }
@@ -346,33 +307,41 @@ public class Board : MonoBehaviour
     }
 
     //get possible available position that card can be plalced on the right of it 
-    public List<Property> GetPossibleRightPosition( ){
+    public List<Property> GetPossibleRightPosition()
+    {
         List<Property> possibleRightList = new List<Property>();
-        foreach(Property prop in usedGridproperty){
-            if(prop.y == 8){
+        foreach (Property prop in usedGridproperty)
+        {
+            if (prop.y == 8)
+            {
                 continue;
             }
-            Property rightGrid = board[prop.x,prop.y+1].GetComponent<Property>();
-            if(prop.Up && !rightGrid.used){//there a way up and up is not used
+            Property rightGrid = board[prop.x, prop.y + 1].GetComponent<Property>();
+            if (prop.Up && !rightGrid.used)
+            {//there a way up and up is not used
                 possibleRightList.Add(rightGrid);
             }
         }
-        return  possibleRightList;
+        return possibleRightList;
     }
 
     //get possible available position that can placed card on the left of it 
-    public List<Property> GetPossibleLeftPosition( ){
+    public List<Property> GetPossibleLeftPosition()
+    {
         List<Property> possibleLeftList = new List<Property>();
-        foreach(Property prop in usedGridproperty){
-            if(prop.y == 0){
+        foreach (Property prop in usedGridproperty)
+        {
+            if (prop.y == 0)
+            {
                 continue;
             }
-            Property leftGrid = board[prop.x,prop.y-1].GetComponent<Property>();
-            if(prop.Up && !leftGrid.used){//there a way up and up is not used
+            Property leftGrid = board[prop.x, prop.y - 1].GetComponent<Property>();
+            if (prop.Up && !leftGrid.used)
+            {//there a way up and up is not used
                 possibleLeftList.Add(leftGrid);
             }
         }
-        return  possibleLeftList;
+        return possibleLeftList;
     }
 
     public bool checkValid(Property c, int x, int y)
@@ -422,7 +391,7 @@ public class Board : MonoBehaviour
             {
                 return false;
             }
-            
+
         }
 
         if (y != 0)
@@ -430,7 +399,7 @@ public class Board : MonoBehaviour
             left = board[x, y - 1].GetComponent<Property>();
             if (left.Right && c.Left)
             {
-                valid = true; Debug.Log("Valid Card Placed at position:" + left.x +" "+ left.y);
+                valid = true; Debug.Log("Valid Card Placed at position:" + left.x + " " + left.y);
 
             }
             if (left.Right != c.Left && left.used)
@@ -445,13 +414,13 @@ public class Board : MonoBehaviour
 
     public void CheckDes(Property prop)
     {
-        if(prop.y == 8 && (prop.x == 0 || prop.x ==2 || prop.x == 4))
+        if (prop.y == 8 && (prop.x == 0 || prop.x == 2 || prop.x == 4))
         {
             //reveal card 
-        }    
+        }
     }
 
 }
 
-    
+
 
