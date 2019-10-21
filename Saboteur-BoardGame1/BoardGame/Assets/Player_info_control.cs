0293ebbe1a53e66731243537758513d9a7800453
mugi0GH <jieng0805@gmail.com>
2019-10-22 07:30:10 +1100

    End_game prefab scripts

---
+++ Saboteur-BoardGame1/BoardGame/Assets/Player_info_control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_info_control : MonoBehaviour
+{
+    public void Row_number(int Row_number)
+    {
+        this.transform.Find("Number").GetComponent<Text>().text = Row_number.ToString();
+    }
+    public void Player_name(string name)
+    {
+        this.transform.Find("Player's name Background/Player's name").GetComponent<Text>().text = name;
+    }
+    public void Total_Gold(int Gold_number)
+    {
+        this.transform.Find("Round/Text").GetComponent<Text>().text = Gold_number.ToString();
+    }
+}
---
+++ Saboteur-BoardGame1/BoardGame/Assets/Round_start_control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Round_start_control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void setPoint(int Point)
+    {
+        this.transform.Find("Score number").GetComponent<Text>().text = Point.ToString();
+    }
+
+
+    public void setRole(string Role)
+    {
+        this.transform.Find("Role").GetComponent<Text>().text = Role;
+    }
+}
